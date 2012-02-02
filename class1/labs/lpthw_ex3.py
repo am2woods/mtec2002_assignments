d705ccdf4a1bc3ac81d7b72ffe91a81bb8c23058
am2woods <am2woods@gmail.com>
2012-02-01 20:22:22 -0500

    Mtec 2002 Assignment 3

---
+++ class1/labs/lpthw_ex3.py
@@ -0,0 +1,27 @@
+# This line will print the following string
+print "I will now count my chickens:"
+
+# This line will assing the 
+print "Hens", 25 + 30 / 6.0
+# This line will
+print "roosters", 100 - 25.0 * 3 % 4
+
+# This line will pring the following string
+print "Now I will count the eggs:"
+
+print 3 + 2.0 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is is true that 3 + 2 < 5 - 5?"
+
+print 3.0 + 2 < 5 - 7
+
+print "what is 3 + 2?", 3 + 2
+print "what is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's false."
+
+print "how about some more?"
+
+print "is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or euqal?", 5 <= -2
