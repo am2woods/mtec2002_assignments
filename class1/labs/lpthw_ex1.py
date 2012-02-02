20a07eefc83f62c35446fad85a17c7221a662e6b
am2woods <am2woods@gmail.com>
2012-02-01 19:10:10 -0500

    MTec 2002 Assignment 1

---
+++ class1/labs/lpthw_ex1.py
@@ -0,0 +1,7 @@
+print "hello world!"
+print "hello again"
+print "I like typing this."
+print "this is fun"
+print "yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
