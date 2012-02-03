051d4f3e903e00bfca492184d448e3ee10b7c365
am2woods <am2woods@gmail.com>
2012-02-03 16:50:27 -0500

    MTEC-2002 Assingnment #29

---
+++ class1/labs/lpthw_ex29.py
@@ -0,0 +1,23 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print "Not many cats! The world is saved!"
+    
+if people < dogs:
+    print "The world is drooled!"
+    
+if people > dogs:
+    print "The workd is dry!"
+
+dogs += 5
+
+if people >= dogs:
+    print "people are greater than or equal to dogs."
+
+if people <= dogs:
+    print "people are less than or equal to dogs."
+
+if people == dogs:
+    print "people are dogs."
